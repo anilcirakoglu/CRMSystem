07c1ea52aa96afbc7acb7388c3cd69e71920f1a2
ANIL CIRAKOGLU <anil.cirakoglu@isik.edu.tr>
2025-03-21 00:53:12 +0300

    Add Context class for db

---
+++ CRMSystem.DataAccessLayer/Concrete/CRMSystemContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMSystem.DataAccessLayer.Concrete
+{
+    public class CRMSystemContext
+    {
+    }
+}
