4ccb8515d1afd735288c16768089cdb5789a302b
ANIL CIRAKOGLU <anil.cirakoglu@isik.edu.tr>
2025-03-21 00:29:40 +0300

    Add User entity

---
+++ CRMSystem.EntityLayer/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMSystem.EntityLayer.Entities
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }= DateTime.Now;
+        public DateTime UpdatedAt { get; set; }
+    }
+}
